054b3ec0db74cab9fc4b9a00a2aba25e9824a479
Justin H <jkh227@nau.edu>
2014-03-30 14:18:36 -0700

    Set up button1 functionality, added popup box

--- src/com/uXperience/jarvis/MainActivity.java
+++ src/com/uXperience/jarvis/MainActivity.java
@@ -1,15 +1,30 @@
 package com.uXperience.jarvis;
-
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.view.Menu;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
+	
+	private Button button1;
+	private View MenuDetails;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//initialize button
+		button1 = (Button)findViewById(R.id.button1);
+		button1.setOnClickListener(this);
+
+		
+		MenuDetails = (View)findViewById(R.id.listview_background_shape);
+
 	}
 
 	@Override
@@ -17,6 +32,15 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v == button1){
+			MenuDetails.setVisibility(View.VISIBLE);
+			button1.setVisibility(View.INVISIBLE);
+		}
+		
 	}
 
 }
