b035d897c9404d553410188bbba400033590acd0
Forrest Townsend <Forrest.town18@gmail.com>
2014-03-31 00:35:49 -0700

    Added new images. New DisplayActivity

---
+++ src/com/uXperience/jarvis/DisplayActivity.java
@@ -0,0 +1,13 @@
+package com.uXperience.jarvis;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Forrest on 3/30/2014.
+ */
+public class DisplayActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
