7072f95e254c7604f4871c7c3bcf4f5dc67627fc
Forrest Townsend <forrest.town18@gmail.com>
2014-04-14 18:46:34 -0700

    Each button on the main page will create an intent to their own activity. Removed all other imagebuttons from home screen

--- src/com/uXperience/jarvis/MainActivity.java
+++ src/com/uXperience/jarvis/MainActivity.java
@@ -2,10 +2,14 @@ package com.uXperience.jarvis;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private ImageButton weatherBtn;
+    private ImageButton musicBtn;
+    private ImageButton mapsBtn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,19 +17,37 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		//initialize items
+        weatherBtn = (ImageButton) findViewById(R.id.weatherButton);
+        weatherBtn.setOnClickListener(this);
 
+        musicBtn = (ImageButton) findViewById(R.id.musicButton);
+        musicBtn.setOnClickListener(this);
+
+        mapsBtn = (ImageButton) findViewById(R.id.mapButton);
+        mapsBtn.setOnClickListener(this);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.main, menu);
+//		return true;
+//	}
 
+    @Override
 	public void onClick(View v) {
-		Intent intent = new Intent(this, DisplayActivity.class);
-        startActivityForResult(intent, CONTEXT_INCLUDE_CODE);
+
+        if(v == weatherBtn) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+        } else if(v == musicBtn) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+        } else if(v == mapsBtn) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+        }
+
 	}
 
 }
