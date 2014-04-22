58cac31dc3869805359c7f466b02066c27d7c764
Forrest Townsend <Forrest.town18@gmail.com>
2014-04-21 17:27:29 -0700

    Trying to get the Camera to work on the Front activity - Adding camera functionality

--- src/com/uXperience/jarvis/MainActivity.java
+++ src/com/uXperience/jarvis/MainActivity.java
@@ -1,9 +1,13 @@
 package com.uXperience.jarvis;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
 public class MainActivity extends Activity implements View.OnClickListener {
@@ -11,6 +15,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private ImageButton weatherBtn;
     private ImageButton musicBtn;
     private ImageButton mapsBtn;
+    private SurfaceView sView;
+
+    private Camera hCamera;
+    private CameraPreview cPreview;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		setContentView(R.layout.activity_main);
 		
 		//initialize items
+        hCamera = getCameraInstance();
+
         weatherBtn = (ImageButton) findViewById(R.id.weatherButton);
         weatherBtn.setOnClickListener(this);
 
@@ -26,7 +36,52 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         mapsBtn = (ImageButton) findViewById(R.id.mapButton);
         mapsBtn.setOnClickListener(this);
+
+        // Create our Preview view and set it as the content of our activity.
+        cPreview = new CameraPreview(this, hCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+
+        sView = (SurfaceView) findViewById(R.id.surfaceView);
+
+        cPreview.surfaceCreated(sView.getHolder());
+        preview.addView(cPreview);
 	}
+
+    private boolean safeCameraOpenInView(View view) {
+        boolean qOpened = false;
+        releaseCameraAndPreview();
+        hCamera = getCameraInstance();
+        qOpened = (hCamera != null);
+        cPreview = new CameraPreview(getActivity().getBaseContext(), hCamera);
+        FrameLayout preview = (FrameLayout) view.findViewById(R.id.camera_preview);
+        preview.addView(cPreview);
+        return qOpened;
+    }
+
+    private void releaseCameraAndPreview() {
+
+        if (hCamera != null) {
+            hCamera.stopPreview();
+            hCamera.release();
+            hCamera = null;
+        }
+        if(cPreview != null){
+            cPreview.destroyDrawingCache();
+            cPreview.hCamera = null;
+        }
+    }
+    
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
 
 //	@Override
 //	public boolean onCreateOptionsMenu(Menu menu) {
