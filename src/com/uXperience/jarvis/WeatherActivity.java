f57cf892da2fa98ff27857f226b129a4a797ecd6
Justin H <jkh227@nau.edu>
2014-04-22 11:44:20 -0700

    Added camera code to weather app.  We need to disable the camera from the main activity before weather can use it.

--- src/com/uXperience/jarvis/WeatherActivity.java
+++ src/com/uXperience/jarvis/WeatherActivity.java
@@ -9,8 +9,20 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.uXperience.jarvis.model.Weather;
+
 import org.json.JSONException;
+
+import android.content.Intent;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 /**
  * Created by Forrest on 4/14/2014.
@@ -26,10 +38,32 @@ public class WeatherActivity extends Activity implements View.OnClickListener{
     private TextView windSpeed;
     private TextView hum;
     private ImageView imgView;
+    private SurfaceView sView;
+    private Camera cameraObject;
+    private ShowCamera showCamera;
+    private ImageView pic;
+    
+    public static Camera isCameraAvailiable(){
+        Camera object = null;
+        try {
+           object = Camera.open(); 
+        }
+        catch (Exception e){
+        }
+        return object; 
+     }
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        
+        /*
+        pic = (ImageView)findViewById(R.id.imageView1);
+	      cameraObject = isCameraAvailiable();
+	      showCamera = new ShowCamera(this, cameraObject);
+	      FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+	      preview.addView(showCamera);
+*/
 
         //initialize items
         exitBtn = (ImageButton) findViewById(R.id.backButton);
